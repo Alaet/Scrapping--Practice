64070a8caa899dd133632bf0f458c3d9cd0cd09a
Alaet <72195619+Alaet@users.noreply.github.com>
2021-11-11 22:58:44 +0100

    Add functions
    ajouts de plusieurs fonction
    !!! ne fonctionne pas sur toutes les pages, a investiguer sur les pages sans pagination !!!

--- P2_oliva_maximilien/P2_02_category.py
+++ P2_oliva_maximilien/P2_02_category.py
@@ -3,19 +3,21 @@ from bs4 import BeautifulSoup
 import csv
 
 """URL's category page"""
-URL = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/"  # !! no index.html on URL variable !!
+URL = "https://books.toscrape.com/catalogue/category/books/historical-fiction_4/"  # !! no index.html on URL variable !!
 urls_page = [URL + "index.html"]
 #https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html
-"""CATEGORY will determine the name of CSV file"""
-CATEGORY = "Fiction"
+#https://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html
 
-NUMBER_OF_PAGE = 10  # Number of page we will search in
-"""Pick up avery product link on given parsed URL page from BeautifoulSoup"""
+
+NUMBER_OF_PAGE = 10  # Number of page searched
+"""Pick up avery product link on given parsed URL page from BeautifulSoup"""
+
 
 def soup_url(url):
     url_reponse = requests.get(url)
     soup = BeautifulSoup(url_reponse.content, 'html.parser')
     return soup
+
 
 def get_all_products_url(soup):
     products_links = []
@@ -26,21 +28,24 @@ def get_all_products_url(soup):
         products_links.append(("https://books.toscrape.com/" + link))
     return products_links
 
+def get_product_upc(soup):
+    upc = soup.find('td')
+    return upc.string
 
-def get_category(urls):
-    #soup = soup_url(urls)
-    #url_reponse = requests.get(urls)
-    #soup = BeautifulSoup(url_reponse.content, 'html.parser')
+def get_category(soup):
     categories = soup.find('ul', class_='breadcrumb').findAll('a')
     return categories[2].string
 
-def get_title(urls):
-    #url_reponse = requests.get(urls)
-    #soup = BeautifulSoup(url_reponse.content, 'html.parser')
+
+def get_title(soup):
     title = soup.find('h1')
     return title.string
 
-with open(CATEGORY+".csv", 'a+', newline='') as fichier_csv:  # Create CSV file
+
+print(urls_page[0])
+soup_title = soup_url(urls_page[0])
+title_csv = get_title(soup_title)
+with open(title_csv+".csv", 'a+', newline='') as fichier_csv:  # Create CSV file
     urls_reponse = []
     urls = []
     """Search for NUMBER_OF_PAGE from this category, index 0 being starting url page category"""
@@ -50,15 +55,16 @@ with open(CATEGORY+".csv", 'a+', newline='') as fichier_csv:  # Create CSV file
         urls_reponse = requests.get(urls_page[i])
         if urls_reponse.ok:
             soup = soup_url(urls_page[i])
-            #soup = BeautifulSoup(urls_reponse.content, 'html.parser')
             urls = get_all_products_url(soup)
             writer = csv.writer(fichier_csv, delimiter=',')
             for i in range(len(urls)):
                 items_url = "https://books.toscrape.com/catalogue/" + urls[i]
                 soup = soup_url(items_url)
+                upc = get_product_upc(soup)
                 categories_url = get_category(soup)
                 titles = get_title(soup)
-                urls[i] = [items_url, categories_url, titles]
+                #ligne = [url, upc, title, price_with_tax, price_without_tax, stock, desc, category, rating, image]
+                urls[i] = [titles, items_url, upc, categories_url]
             for row in urls:
                 writer.writerow(row)
         else:
