c063d7a4d67bd387aa6adf898c720763eb2d9948
Alaet <72195619+Alaet@users.noreply.github.com>
2021-11-11 21:55:37 +0100

    Update P2_02_category.py
    maj de l'integration des données dans le csv + integration de quelques fonctions

--- P2_oliva_maximilien/P2_02_category.py
+++ P2_oliva_maximilien/P2_02_category.py
@@ -3,15 +3,19 @@ from bs4 import BeautifulSoup
 import csv
 
 """URL's category page"""
-
-urls_page = ["https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"]
-
+URL = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/"  # !! no index.html on URL variable !!
+urls_page = [URL + "index.html"]
+#https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html
 """CATEGORY will determine the name of CSV file"""
 CATEGORY = "Fiction"
 
 NUMBER_OF_PAGE = 10  # Number of page we will search in
 """Pick up avery product link on given parsed URL page from BeautifoulSoup"""
 
+def soup_url(url):
+    url_reponse = requests.get(url)
+    soup = BeautifulSoup(url_reponse.content, 'html.parser')
+    return soup
 
 def get_all_products_url(soup):
     products_links = []
@@ -23,23 +27,39 @@ def get_all_products_url(soup):
     return products_links
 
 
-with open(CATEGORY+".csv", 'a+') as fichier_csv:  # Create CSV file
+def get_category(urls):
+    #soup = soup_url(urls)
+    #url_reponse = requests.get(urls)
+    #soup = BeautifulSoup(url_reponse.content, 'html.parser')
+    categories = soup.find('ul', class_='breadcrumb').findAll('a')
+    return categories[2].string
+
+def get_title(urls):
+    #url_reponse = requests.get(urls)
+    #soup = BeautifulSoup(url_reponse.content, 'html.parser')
+    title = soup.find('h1')
+    return title.string
+
+with open(CATEGORY+".csv", 'a+', newline='') as fichier_csv:  # Create CSV file
     urls_reponse = []
     urls = []
-    """Search for 10 pages from this category, index 0 being starting url page category"""
+    """Search for NUMBER_OF_PAGE from this category, index 0 being starting url page category"""
     for i in range(1, NUMBER_OF_PAGE):
-        urls_reponse = urls_page.append("https://books.toscrape.com/catalogue/category/books/mystery_3/page-" + str(i) +
+        urls_reponse = urls_page.append(URL + "page-" + str(i) +
                                         ".html")
         urls_reponse = requests.get(urls_page[i])
-
         if urls_reponse.ok:
-            soup = BeautifulSoup(urls_reponse.content, 'html.parser')
-            # for i in range(len(urls_response)):
+            soup = soup_url(urls_page[i])
+            #soup = BeautifulSoup(urls_reponse.content, 'html.parser')
             urls = get_all_products_url(soup)
             writer = csv.writer(fichier_csv, delimiter=',')
             for i in range(len(urls)):
-                items = "https://books.toscrape.com/catalogue/" + urls[i]
-                urls[i] = items
-            writer.writerow(urls)
+                items_url = "https://books.toscrape.com/catalogue/" + urls[i]
+                soup = soup_url(items_url)
+                categories_url = get_category(soup)
+                titles = get_title(soup)
+                urls[i] = [items_url, categories_url, titles]
+            for row in urls:
+                writer.writerow(row)
         else:
             pass
