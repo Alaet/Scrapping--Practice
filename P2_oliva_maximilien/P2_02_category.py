5b0beed00c8de2be10826a9cab64098275552824
Alaet <72195619+Alaet@users.noreply.github.com>
2021-11-12 11:08:00 +0100

    Update P2_02_category.py
    working, need fix and cleaning

--- P2_oliva_maximilien/P2_02_category.py
+++ P2_oliva_maximilien/P2_02_category.py
@@ -3,13 +3,14 @@ from bs4 import BeautifulSoup
 import csv
 
 """URL's category page"""
-URL = "https://books.toscrape.com/catalogue/category/books/historical-fiction_4/"  # !! no index.html on URL variable !!
+URL = "https://books.toscrape.com/catalogue/category/books/travel_2/"  # !! no index.html on URL variable !!
 urls_page = [URL + "index.html"]
-#https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html
-#https://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html
+#  https://books.toscrape.com/catalogue/category/books/sequential-art_5/
+#  https://books.toscrape.com/catalogue/category/books/historical-fiction_4/
+#  https://books.toscrape.com/catalogue/category/books/travel_2/
 
-
-NUMBER_OF_PAGE = 10  # Number of page searched
+NUMBER_OF_PAGE = 1  # Number of page searched + 1
+j = 0
 """Pick up avery product link on given parsed URL page from BeautifulSoup"""
 
 
@@ -28,9 +29,11 @@ def get_all_products_url(soup):
         products_links.append(("https://books.toscrape.com/" + link))
     return products_links
 
+
 def get_product_upc(soup):
     upc = soup.find('td')
     return upc.string
+
 
 def get_category(soup):
     categories = soup.find('ul', class_='breadcrumb').findAll('a')
@@ -42,29 +45,77 @@ def get_title(soup):
     return title.string
 
 
-print(urls_page[0])
+def get_price_with_tax(soup):
+    prices = soup.findAll('td')
+    return prices[3].text
+
+
+def get_price_without_tax(soup):
+    prices = soup.findAll('td')
+    return prices[2].text
+
+
+def get_stock(soup):
+    number_in_stock = soup.findAll('td')
+    return number_in_stock[5].string
+
+
+def get_product_description(soup):
+    descriptions = soup.findAll('p')
+    return descriptions[3].string
+
+
+def get_rating_review(soup):
+    rating = soup.find('div', class_='col-sm-6 product_main').findAll('p')
+    return rating[2]['class'][1]
+
+
+def get_img_url(soup):
+    images = soup.find('div', class_='item active')
+    links = []
+    a = images.find('img')
+    link = a['src']
+    links.append('https://books.toscrape.com/' + link)
+    return links[0]
+
+if NUMBER_OF_PAGE > 1:
+    j = 1
 soup_title = soup_url(urls_page[0])
 title_csv = get_title(soup_title)
-with open(title_csv+".csv", 'a+', newline='') as fichier_csv:  # Create CSV file
+en_tete = ["TITRE", "PRODUCT_PAGE_URL", "UPC", "PRICE_INCLUDING_TAX", "PRICE_EXCLUDING_TAX", "NUMBER_AVAILABLE",
+                    "PRODUCT_DESCRIPTION", "CATEGORY", "REVIEW_RATING", "IMAGE_URL"]
+with open(title_csv+".csv", 'a+', encoding='utf-8', newline='') as fichier_csv:  # Create CSV file
     urls_reponse = []
     urls = []
+    writer = csv.writer(fichier_csv, delimiter=',')
+
     """Search for NUMBER_OF_PAGE from this category, index 0 being starting url page category"""
-    for i in range(1, NUMBER_OF_PAGE):
+    for i in range(j, NUMBER_OF_PAGE):
+        writer.writerow(en_tete)
+        writer.writerow("")
         urls_reponse = urls_page.append(URL + "page-" + str(i) +
                                         ".html")
         urls_reponse = requests.get(urls_page[i])
         if urls_reponse.ok:
-            soup = soup_url(urls_page[i])
+            soup = BeautifulSoup(urls_reponse.content, 'html.parser')
             urls = get_all_products_url(soup)
-            writer = csv.writer(fichier_csv, delimiter=',')
-            for i in range(len(urls)):
+            soup = soup_url(urls_page[i])
+            for i in range(0, len(urls)):
                 items_url = "https://books.toscrape.com/catalogue/" + urls[i]
+                print(urls[i])
                 soup = soup_url(items_url)
                 upc = get_product_upc(soup)
                 categories_url = get_category(soup)
                 titles = get_title(soup)
+                price_with_tax = get_price_with_tax(soup)
+                price_without_tax = get_price_without_tax(soup)
+                stock = get_stock(soup)
+                desc = get_product_description(soup)
+                rating = get_rating_review(soup)
+                image = get_img_url(soup)
                 #ligne = [url, upc, title, price_with_tax, price_without_tax, stock, desc, category, rating, image]
-                urls[i] = [titles, items_url, upc, categories_url]
+                urls[i] = [titles, items_url, upc, price_with_tax, price_without_tax, stock, desc, categories_url,
+                           rating, image]
             for row in urls:
                 writer.writerow(row)
         else:
