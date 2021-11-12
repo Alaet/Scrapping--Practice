13f63f35d833b8812c3960f8349bd7a14b167712
Alaet <72195619+Alaet@users.noreply.github.com>
2021-11-12 14:04:24 +0100

    Code cleaning
    Fix petit bug sur script category et nettoyage du code du script bookpage

--- P2_oliva_maximilien/P2_01_bookpage.py
+++ P2_oliva_maximilien/P2_01_bookpage.py
@@ -1,80 +1,93 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from datetime import date
+
+"""URL's book page,    !SET BY USER!  """
+url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+
+"""Reach and Check user url's response, if it can't be reached, will prompt an error message in console"""
+response = requests.get(url)
+if response.ok:
+    soup = BeautifulSoup(response.content, 'html.parser')
+else:
+    print("Impossible de joindre la page, vérifier l'URL")
 
 
-def main():
-    url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-    response = requests.get(url)
+# region All functions for each informations
 
-    if response.ok:
-        soup = BeautifulSoup(response.content, 'html.parser')
-    else:
-        print("Impossible de joindre la page")
-
-    # region All functions for each informations
-
-    def get_title():
-        title = soup.find('h1')
-        return title.string
-
-    def get_product_upc():
-        upc = soup.find('td')
-        return upc.string
-
-    def get_price_with_tax():
-        prices = soup.findAll('td')
-        return prices[3].text
-
-    def get_price_without_tax():
-        prices = soup.findAll('td')
-        return prices[2].text
-
-    def get_stock():
-        number_in_stock = soup.findAll('td')
-        return number_in_stock[5].string
-
-    def get_product_description():
-        descriptions = soup.findAll('p')
-        return descriptions[3].string
-
-    def get_category():
-        categories = soup.find('ul', class_='breadcrumb').findAll('a')
-        return categories[2].string
-
-    def get_rating_review():
-        rating = soup.find('div', class_='col-sm-6 product_main').findAll('p')
-        return rating[2]['class'][1]
-
-    def get_img_url():
-        images = soup.find('div', class_='item active')
-        links = []
-        a = images.find('img')
-        link = a['src']
-        links.append('https://books.toscrape.com/' + link)
-        return links[0]
-
-    # endregion
-
-    csv_title = get_title()
-    en_tete = ["product_page_url", "upc", "title", "price_including_tax", "price_excluding_tax", "number_available",
-               "product_description", "category", "review_rating", "image_url"]
-    with open(csv_title+".csv", 'a+') as fichier_csv:
-        writer = csv.writer(fichier_csv, delimiter=',')
-        writer.writerow(en_tete)
-        title = get_title()
-        image = get_img_url()
-        desc = get_product_description()
-        rating = get_rating_review()
-        category = get_category()
-        stock = get_stock()
-        price_with_tax = str(get_price_with_tax())
-        price_without_tax = str(get_price_without_tax())
-        upc = get_product_upc()
-        url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-        ligne = [url, upc, title, price_with_tax, price_without_tax, stock, desc, category, rating, image]
-        writer.writerow(ligne)
+def get_title():
+    title = soup.find('h1')
+    return title.string
 
 
-if __name__ == '__main__':
-    main()
+def get_product_upc():
+    upc = soup.find('td')
+    return upc.string
+
+
+def get_price_with_tax():
+    price = soup.findAll('td')
+    return price[3].text
+
+
+def get_price_without_tax():
+    price = soup.findAll('td')
+    return price[2].text
+
+
+def get_stock():
+    number_in_stock = soup.findAll('td')
+    return number_in_stock[5].string
+
+
+def get_product_description():
+    description = soup.findAll('p')
+    return description[3].string
+
+
+def get_category():
+    categorie = soup.find('ul', class_='breadcrumb').findAll('a')
+    return categorie[2].string
+
+
+def get_rating_review():
+    rating_stars = soup.find('div', class_='col-sm-6 product_main').findAll('p')
+    return rating_stars[2]['class'][1]
+
+
+def get_img_url():
+    image_div = soup.find('div', class_='item active')
+    image_url = []
+    a = image_div.find('img')
+    link_url = a['src']
+    image_url.append('https://books.toscrape.com/' + link_url)
+    return image_url[0]
+
+
+# endregion
+
+# region CSV file creation, completion with name: [Book's title + DateOfTheDay].csv
+
+csv_title = get_title() + " " + date.today().strftime("%b-%d-%Y")
+header = ["PRODUCT_PAGE_URL", "UPC", "TITLE", "PRICE_INCLUDING_TAX", "PRICE_EXCLUDING_TAX", "NUMBER_AVAILABLE",
+          "PRODUCT_DESCRIPTION", "CATEGORY", "REVIEW_RATING", "IMAGE_URL"]
+
+"""CSV creation"""
+with open(csv_title + ".csv", 'a+', encoding='utf-8') as fichier_csv:
+    writer = csv.writer(fichier_csv, delimiter=',')
+    writer.writerow(header)
+
+    title = get_title()
+    image = get_img_url()
+    desc = get_product_description()
+    rating = get_rating_review()
+    category = get_category()
+    stock = get_stock()
+    price_with_tax = str(get_price_with_tax())
+    price_without_tax = str(get_price_without_tax())
+    upc = get_product_upc()
+    #url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+    csv_line = [url, upc, title, price_with_tax, price_without_tax, stock, desc, category, rating, image]
+    writer.writerow(csv_line)
+# endregion
--- P2_oliva_maximilien/P2_02_category.py
+++ P2_oliva_maximilien/P2_02_category.py
@@ -1,19 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from datetime import date
 
-"""URL's category page"""
-URL = "https://books.toscrape.com/catalogue/category/books/travel_2/"  # !! no index.html on URL variable !!
-urls_page = [URL + "index.html"]
-#  https://books.toscrape.com/catalogue/category/books/sequential-art_5/
-#  https://books.toscrape.com/catalogue/category/books/historical-fiction_4/
-#  https://books.toscrape.com/catalogue/category/books/travel_2/
+"""URL's category page,    !SET BY USER!  """
+URL_CATEGORY = "https://books.toscrape.com/catalogue/category/books/travel_2/"  # !! NO INDEX.HTML ON URL VARIABLE !!
 
-NUMBER_OF_PAGE = 1  # Number of page searched + 1
-j = 0
+
+"""Variables that will determine how many page to search,    !SET BY USER!  """
+NUMBER_OF_BOOKS_RESULTS = 11
+NUMBER_OF_BOOKS_SHOWING = 0
+
+
 """Pick up avery product link on given parsed URL page from BeautifulSoup"""
 
-
+# region All functions for each informations
 def soup_url(url):
     url_reponse = requests.get(url)
     soup = BeautifulSoup(url_reponse.content, 'html.parser')
@@ -33,11 +34,6 @@ def get_all_products_url(soup):
 def get_product_upc(soup):
     upc = soup.find('td')
     return upc.string
-
-
-def get_category(soup):
-    categories = soup.find('ul', class_='breadcrumb').findAll('a')
-    return categories[2].string
 
 
 def get_title(soup):
@@ -77,35 +73,45 @@ def get_img_url(soup):
     link = a['src']
     links.append('https://books.toscrape.com/' + link)
     return links[0]
+# endregion
 
-if NUMBER_OF_PAGE > 1:
+# region CSV environment preparation with name: [Category title + DateOfTheDay].csv
+
+
+urls_page_category = [URL_CATEGORY + "index.html"]
+if NUMBER_OF_BOOKS_SHOWING != 0:
+    NUMBER_OF_PAGE = int((NUMBER_OF_BOOKS_RESULTS/NUMBER_OF_BOOKS_SHOWING) + 2)
     j = 1
-soup_title = soup_url(urls_page[0])
-title_csv = get_title(soup_title)
+else:
+    NUMBER_OF_PAGE = 1
+    j = 0
+
+soup_title = soup_url(urls_page_category[0])
+title_csv = get_title(soup_title) + " " + date.today().strftime("%b-%d-%Y")
 en_tete = ["TITRE", "PRODUCT_PAGE_URL", "UPC", "PRICE_INCLUDING_TAX", "PRICE_EXCLUDING_TAX", "NUMBER_AVAILABLE",
-                    "PRODUCT_DESCRIPTION", "CATEGORY", "REVIEW_RATING", "IMAGE_URL"]
+                    "PRODUCT_DESCRIPTION", "REVIEW_RATING", "IMAGE_URL"]
+# endregion
+
+# region CSV file creation, completion
 with open(title_csv+".csv", 'a+', encoding='utf-8', newline='') as fichier_csv:  # Create CSV file
     urls_reponse = []
     urls = []
     writer = csv.writer(fichier_csv, delimiter=',')
-
+    writer.writerow(en_tete)
+    writer.writerow("")
     """Search for NUMBER_OF_PAGE from this category, index 0 being starting url page category"""
     for i in range(j, NUMBER_OF_PAGE):
-        writer.writerow(en_tete)
-        writer.writerow("")
-        urls_reponse = urls_page.append(URL + "page-" + str(i) +
-                                        ".html")
-        urls_reponse = requests.get(urls_page[i])
+        urls_reponse = urls_page_category.append(URL_CATEGORY + "page-" + str(i) + ".html")
+        urls_reponse = requests.get(urls_page_category[i])
         if urls_reponse.ok:
             soup = BeautifulSoup(urls_reponse.content, 'html.parser')
             urls = get_all_products_url(soup)
-            soup = soup_url(urls_page[i])
+            soup = soup_url(urls_page_category[i])
             for i in range(0, len(urls)):
                 items_url = "https://books.toscrape.com/catalogue/" + urls[i]
                 print(urls[i])
                 soup = soup_url(items_url)
                 upc = get_product_upc(soup)
-                categories_url = get_category(soup)
                 titles = get_title(soup)
                 price_with_tax = get_price_with_tax(soup)
                 price_without_tax = get_price_without_tax(soup)
@@ -113,10 +119,9 @@ with open(title_csv+".csv", 'a+', encoding='utf-8', newline='') as fichier_csv: 
                 desc = get_product_description(soup)
                 rating = get_rating_review(soup)
                 image = get_img_url(soup)
-                #ligne = [url, upc, title, price_with_tax, price_without_tax, stock, desc, category, rating, image]
-                urls[i] = [titles, items_url, upc, price_with_tax, price_without_tax, stock, desc, categories_url,
-                           rating, image]
+                urls[i] = [titles, items_url, upc, price_with_tax, price_without_tax, stock, desc, rating, image]
             for row in urls:
                 writer.writerow(row)
         else:
             pass
+# endregion
