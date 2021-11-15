882e8e712315e1ed88445519a022fa2b9eb1a6b6
Alaet <72195619+Alaet@users.noreply.github.com>
2021-11-15 16:50:40 +0100

    Update P2_03_BookScrap.py
    Appli OK
    Renaming of most variables for clarity
    ToDo : README

--- P2_oliva_maximilien/P2_03_BookScrap.py
+++ P2_oliva_maximilien/P2_03_BookScrap.py
@@ -4,97 +4,116 @@ import csv
 from datetime import date
 import time
 import urllib.request
+import os
+
+# region Create directory Book_Images in current script directory
+current_directory = os.getcwd()
+print(current_directory)
+book_image_directory = os.path.join(current_directory + '/Book_images/')
+print(book_image_directory)
+if not os.path.exists(book_image_directory):
+    os.makedirs(book_image_directory)
+# endregion
+
+# region Functions for each information, for any book's url provided
+
+"""Parse any url given and return the result for use"""
 
 
-
-# !! NO INDEX.HTML ON URL VARIABLE !!
-
-
-# region All functions for each informations
-
-
-def soup_url(url):
-    url_reponse = requests.get(url)
-    soup = BeautifulSoup(url_reponse.content, 'html.parser')
-    return soup
+def soup_url(given_url):
+    given_url_response = requests.get(given_url)
+    given_url_soup = BeautifulSoup(given_url_response.content, 'html.parser')
+    return given_url_soup
 
 
-def get_all_products_url(soup):
+"""Return URL's list for every book's url"""
+
+
+def get_all_products_url(given_soup):
     books_links = []
-    book_url = soup.findAll('h3')
-    for url in book_url:
-        a = url.find('a')
+    books_url = given_soup.findAll('h3')
+    for url_book in books_url:
+        a = url_book.find('a')
         link_url = a['href']
         books_links.append(("https://books.toscrape.com/" + link_url))
     return books_links
 
 
-def get_product_upc(soup):
-    upc = soup.find('td')
-    return upc.string
+def get_product_upc(given_soup):
+    given_soup_upc = given_soup.find('td')
+    return given_soup_upc.string
 
 
-def get_title(soup):
-    title = soup.find('h1')
-    return title.string
+def get_title(given_soup):
+    given_soup_title = given_soup.find('h1')
+    return given_soup_title.string
 
 
-def get_price_with_tax(soup):
-    price = soup.findAll('td')
-    return price[3].text
+def get_price_with_tax(given_soup):
+    given_soup_price = given_soup.findAll('td')
+    return given_soup_price[3].text
 
 
-def get_price_without_tax(soup):
-    price = soup.findAll('td')
-    return price[2].text
+def get_price_without_tax(given_soup):
+    given_soup_price = given_soup.findAll('td')
+    return given_soup_price[2].text
 
 
-def get_stock(soup):
-    number_in_stock = soup.findAll('td')
-    return number_in_stock[5].string
+def get_stock(given_soup):
+    given_soup_number_in_stock = given_soup.findAll('td')
+    return given_soup_number_in_stock[5].string
 
 
-def get_product_description(soup):
-    description = soup.findAll('p')
-    return description[3].string
+def get_product_description(given_soup):
+    given_soup_description = given_soup.findAll('p')
+    return given_soup_description[3].string
 
 
-def get_rating_review(soup):
-    rating_stars = soup.find('div', class_='col-sm-6 product_main').findAll('p')
-    return rating_stars[2]['class'][1]
+def get_rating_review(given_soup):
+    given_soup_rating_stars = given_soup.find('div', class_='col-sm-6 product_main').findAll('p')
+    return given_soup_rating_stars[2]['class'][1]
+
+"""Return Image URL from a page book"""
 
 
-def get_img_url(soup):
-    image_div = soup.find('div', class_='item active')
-    image_url = []
+def get_img_url(given_soup):
+    image_div = given_soup.find('div', class_='item active')
+    given_soup_image_url = []
     a = image_div.find('img')
     link_url = a['src']
-    image_url.append('https://books.toscrape.com/' + link_url)
-    return image_url[0]
+    given_soup_image_url.append('https://books.toscrape.com/' + link_url)
+    return given_soup_image_url[0]
 # endregion
+
+# region Get every categories on main page and return every url as a list : categories_url"""
+
+
 URL = "https://books.toscrape.com/index.html"
 soup = soup_url(URL)
 category_div = soup.find(class_='nav nav-list')
 categories_urls = []
 links_url = category_div.findAll('a')
+
 for url in links_url:
 
     link = str(url['href'])
     categories_urls.append("https://books.toscrape.com/" + link.replace('index.html', ''))
 
 categories_urls.pop(0)
-for url in categories_urls:
-    """URL's category page,    !SET BY USER!  """
-    URL_CATEGORY = url
-    # region CSV environment preparation with name: [Category title + DateOfTheDay].csv
+# endregion
 
+# region CSV environment preparation with name: [Category title + DateOfTheDay].csv
+
+for url in categories_urls:
+
+    """URL's category page, from list categories_urls"""
+    URL_CATEGORY = url
 
     urls_page_category = [URL_CATEGORY + "index.html"]
 
-    """Get category from url set by user to determine and set CSV file title"""
-
-    soup_title = soup_url(urls_page_category[0])
-    div_nb = soup_title.find('div', class_='col-sm-8 col-md-9')
+    """Determine numbers of books in category to fix number of pages to search"""
+    soup_category_url = soup_url(urls_page_category[0])
+    div_nb = soup_category_url.find('div', class_='col-sm-8 col-md-9')
     nbs = div_nb.findAll('strong')
     NUMBER_OF_BOOKS_RESULTS = int(nbs[0].string)
     if len(nbs) > 2:
@@ -104,10 +123,11 @@ for url in categories_urls:
     else:
         NUMBER_OF_PAGE = 1
         j = 0
-    title_csv = get_title(soup_title) + " " + date.today().strftime("%b-%d-%Y") + ".csv"
+
+    """Set CSV title based on current category"""
+    title_csv = get_title(soup_category_url) + " " + date.today().strftime("%b-%d-%Y") + ".csv"
     en_tete = ["TITRE", "PRODUCT_PAGE_URL", "UPC", "PRICE_INCLUDING_TAX", "PRICE_EXCLUDING_TAX", "NUMBER_AVAILABLE",
                         "PRODUCT_DESCRIPTION", "REVIEW_RATING", "IMAGE_URL"]
-    # endregion
 
     # region CSV file creation, completion
     with open(title_csv, 'a+', encoding='utf-8', newline='') as fichier_csv:  # Create CSV file
@@ -143,8 +163,16 @@ for url in categories_urls:
                     """Add every information for each book to urls_book list from book_url"""
                     urls_book[k] = [title, book_url, upc, price_with_tax, price_without_tax, stock, description,
                                     rating_stars, image_url]
-                    urllib.request.urlretrieve(image_url.replace("../../", ''), (title.replace(":", '').replace('.', '')
-                                                                                 .replace('?', '') + '.jpg'))
+
+                    """Save image from url"""
+                    urllib.request.urlretrieve(image_url.replace("../../", ''), os.path.join(book_image_directory,
+                                                                                             (title.replace(":", '')
+                                                                                              .replace('.', '')
+                                                                                              .replace('?', '')
+                                                                                              .replace('/', '')
+                                                                                              .replace('"', '')
+                                                                                              .replace('*', '')
+                                                                                              + '.jpg')))
                     print(title)
                     time.sleep(1)
                 for row in urls_book:
@@ -156,3 +184,4 @@ for url in categories_urls:
                 pass
         print(title_csv + " a bien été extrait.")
     # endregion
+# endregion
