045320c44797cb1d9ab76d5cfc0e39950303c0f3
Alaet <72195619+Alaet@users.noreply.github.com>
2021-12-12 11:22:19 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from itertools import product
-
-b = np.array([[1],[1]])
-
-r=[-1,0,1];b=(2==(n:=np.array([np.roll(b,s,r[1:])for s in filter(any,product(r,r))],dtype=int).sum(0)))&b|(n==3)
